dcbbfc67a1cdf146532dc54b139ebb811b53b268
Michael Jordan <michdan25@gmail.com>
2021-04-21 14:06:54 +1200

    projectile spawn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,38 +5,70 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int ID;
+    public Vector2 maxDist;
+    public Vector2 minDist;
+    public GameObject[] projectileSpawnLoc;
 
     private Rigidbody body;
-    private float speed = 0.0f;
-    private float maxSpeed = 10.0f;
-    private float acceleration = 5.0f;
-    private float deceleration = 8.0f;
+    
+    private float speed = 100.0f;
+    private float rotationSpeed = 100.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody>();
+        projectileSpawnLoc[0].AddComponent<BasicShotType>();
+        projectileSpawnLoc[1].AddComponent<BasicShotType>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalAxis = InputManager.instance.GetHorizontalInput(ID);
+        Vector3 targetLoc = transform.position;
+
+        if (transform.position.x < minDist.x)
+        {
+            targetLoc.x = maxDist.x;
+        }
+        if (transform.position.x > maxDist.x)
+        {
+            targetLoc.x = minDist.x;
+        }
+        if (transform.position.y < minDist.y)
+        {
+            targetLoc.y = maxDist.y;
+        }
+        if (transform.position.y > maxDist.y)
+        {
+            targetLoc.y = minDist.y;
+        }
+
+        if (targetLoc != transform.position)
+        {
+            transform.position = targetLoc;
+        }
+
+        if (InputManager.instance.GetPlayerShoot(ID))
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                projectileSpawnLoc[i].GetComponent<BasicShotType>().Fire();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
         float verticalAxis = InputManager.instance.GetVerticalInput(ID);
-        
-        transform.Rotate(new Vector3(0.0f, 0.0f, Mathf.Deg2Rad * -10.0f * horizontalAxis), Space.Self);
-        
-        if(verticalAxis > 0.0f)
+        float horizontalAxis = InputManager.instance.GetHorizontalInput(ID);
+
+        if (verticalAxis > 0.0f)
         {
-            speed += acceleration * Time.deltaTime;
-        }
-        else if (verticalAxis < 0.0f)
-        {
-            speed -= deceleration * Time.deltaTime;
+            body.AddForce(transform.up * speed * verticalAxis * Time.deltaTime, ForceMode.Acceleration);
         }
 
-        speed = Mathf.Clamp(speed, 0.0f, maxSpeed);
+        body.rotation = Quaternion.Euler(body.rotation.eulerAngles + new Vector3(0.0f, 0.0f, Mathf.Deg2Rad * -rotationSpeed * horizontalAxis));
 
-        transform.position += transform.up * speed * Time.deltaTime;
     }
 }
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PowerUp
+{
+    public abstract class GunType : MonoBehaviour
+    {
+        //public abstract void Fire(ShotType type);
+        public abstract void Fire();
+    }
+    public abstract class ShotType : MonoBehaviour
+    {
+        public abstract void Spawn(Vector3 origin, Vector3 direction);
+        protected void OnCollisionEnter(Collision collision) { }
+    }
+}
+
