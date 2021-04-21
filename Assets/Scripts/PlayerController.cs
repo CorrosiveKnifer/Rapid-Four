a7688801a91f67527a67598e8a45bc356ccf12b0
Michael Jordan <michdan25@gmail.com>
2021-04-21 12:37:39 +1200

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody body;
     private float speed = 0.0f;
+    private float maxSpeed = 10.0f;
     private float acceleration = 5.0f;
     private float deceleration = 8.0f;
 
@@ -25,14 +26,16 @@ public class PlayerController : MonoBehaviour
         
         transform.Rotate(new Vector3(0.0f, 0.0f, Mathf.Deg2Rad * 10.0f * horizontalAxis), Space.Self);
         
-        if(verticalAxis > 1.0f)
+        if(verticalAxis > 0.0f)
         {
             speed += acceleration * Time.deltaTime;
         }
-        else
+        else if (verticalAxis < 0.0f)
         {
             speed -= deceleration * Time.deltaTime;
         }
+
+        speed = Mathf.Clamp(speed, 0.0f, maxSpeed);
 
         transform.position += transform.up * speed * Time.deltaTime;
     }
