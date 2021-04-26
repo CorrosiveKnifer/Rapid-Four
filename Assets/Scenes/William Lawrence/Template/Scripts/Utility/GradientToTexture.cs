bd3a83cbaaab561de2ace6a1761a20319194c077
William Lawrence <80514143+Will-3D@users.noreply.github.com>
2021-04-26 14:50:32 +1200

    Shield effect

---
+++ Assets/Scenes/William Lawrence/Template/Scripts/Utility/GradientToTexture.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Sirenix.OdinInspector;
+using System.IO;
+
+[ExecuteAlways]
+public class GradientToTexture : MonoBehaviour
+{
+#if UNITY_EDITOR
+    public string m_ExportPath;
+    public string m_ExportName;
+
+    public int m_TextureResolution;
+
+    public Gradient m_TextureGradient;
+
+    public bool m_Export;
+
+    private void Update()
+    {
+        if (m_Export)
+        {
+            Export();
+            m_Export = false;
+        }
+    }
+
+    private void Export()
+    {
+        Texture2D tex = new Texture2D(m_TextureResolution, m_TextureResolution, TextureFormat.ARGB32, true);
+
+        Color[] colours = new Color[m_TextureResolution * m_TextureResolution];
+        for (int i = 0; i < m_TextureResolution; i++)
+        {
+            Color col = m_TextureGradient.Evaluate((float)i / (float)(m_TextureResolution - 1));
+            for (int w = 0; w < m_TextureResolution; w++)
+            {
+                colours[(i) * m_TextureResolution + w] = col;
+            }
+        }
+        tex.SetPixels(colours);
+        tex.Apply();
+
+        byte[] texBytes = tex.EncodeToPNG();
+        DestroyImmediate(tex);
+
+        File.WriteAllBytes(m_ExportPath + m_ExportName + ".png", texBytes);
+        System.GC.Collect();
+
+        UnityEditor.AssetDatabase.Refresh();
+    }
+#endif
+}
---
+++ Assets/Scenes/William Lawrence/Template/Scripts/VisualEffectManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisualEffectManager : MonoBehaviour
+{
+    [Header("Attached Animator")]
+    public Animator m_Animator;
+
+    [Header("Camera Shake")]
+    public float m_CameraShake = 0.0f;
+    public float m_CameraShakeStrength = 4.0f;
+    public Camera m_AttachedCamera;
+
+    [Header("UI Elements")]
+    public Button m_FireButton;
+
+    private void ShakeCamera()
+    {
+        float x = Random.Range(0.0f, 360.0f);
+        float y = Random.Range(0.0f, 360.0f);
+        float z = Random.Range(0.0f, 360.0f);
+        Vector3 shake = Quaternion.Euler(x, y, z) * Vector3.forward;
+        m_AttachedCamera.transform.position += shake * m_CameraShakeStrength * m_CameraShake * Time.deltaTime;
+    }
+
+    public void Fire()
+    {
+        if (m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Fire"))
+        {
+            return;
+        }
+
+        m_Animator.SetTrigger("Fire");
+    }
+
+    private void Update()
+    {
+        m_FireButton.interactable = !m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Fire");
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Fire();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        ShakeCamera();
+    }
+}
