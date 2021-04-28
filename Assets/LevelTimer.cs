9b8648f9780d3a0018c640ea725c4df1c3ef5dfc
Michael Jordan <michdan25@gmail.com>
2021-04-28 23:06:15 +1200

    cinematic go bur

--- Assets/LevelTimer.cs
+++ Assets/LevelTimer.cs
@@ -8,7 +8,7 @@ public class LevelTimer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<AudioAgent>().PlayBackground("CutSceneSpaceShips", false, 10);
     }
 
     // Update is called once per frame
