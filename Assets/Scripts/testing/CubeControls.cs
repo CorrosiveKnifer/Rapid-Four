07d3e4fdea65b9f504b7e8581820aad06e418d7d
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2021-06-10 16:42:23 +1200

    seperate inputs

--- Assets/Scripts/testing/CubeControls.cs
+++ Assets/Scripts/testing/CubeControls.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// THIS IS FOR TESTING PURPOSES
+/// The allGamePad variable is the primary control testing
+/// The controls is the alternative method we will use
+/// Allgamepad contains the gamepads of the controlls in the system
+/// </summary>
 public class CubeControls : MonoBehaviour
 {
     ControlInput controls;
@@ -11,33 +17,31 @@ public class CubeControls : MonoBehaviour
     private Rigidbody body;
     public float speed = 350.0f;
     public float rotationSpeed = 120.0f;
+    Gamepad player1;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        var allGamepads = Gamepad.all;
+        player1 = allGamepads[0];
         
-        Movement();
+        float here = allGamepads.Count;
+        Debug.Log(player1);
+
+        //Debug.Log(string.Join("\n", Gamepad.all));
     }
 
+    /*
     private void Awake()
     {
         controls = new ControlInput();
 
-        //controls.Gameplay.Rotate.performed += ctx => rotate = ctx.ReadValue<Vector2>();
-        //controls.Gameplay.Rotate.canceled += ctx => rotate = Vector2.zero;
 
         controls.Gameplay.Shoot.performed += ctx => Shooting();
         controls.Gameplay.Rotate.performed += ctx => rotate = ctx.ReadValue<Vector2>();
-        //controls.Gameplay.Rotate.canceled += ctx => rotate = Vector2.zero;
 
         controls.Gameplay.Thrust.performed += ctx => thrust = ctx.ReadValue<Vector2>();
         controls.Gameplay.Thrust.canceled += ctx => thrust = Vector2.zero;
-        //Debug.Log("Shoot");
     }
 
     private void OnEnable()
@@ -52,6 +56,12 @@ public class CubeControls : MonoBehaviour
     {
         Debug.Log("Pew");
     }
+    */
+
+    /// <summary>
+    /// A function that is called in update when the right joysticks move
+    /// Works for controls Input system
+    /// </summary>
     private void Movement()
     {
         Vector2 r = new Vector2(rotate.x, rotate.y);
@@ -63,32 +73,57 @@ public class CubeControls : MonoBehaviour
         Vector2 m = new Vector2(-thrust.x, thrust.y);
 
         float horizontalAxis = m.x;
-        float verticalAxis = m.y;
+        float verticalAxis = m.y
+            ;
         if (verticalAxis > 0.0f)
         {
-            body.AddForce(transform.up * speed * verticalAxis * Time.deltaTime, ForceMode.Acceleration);
-   
+            //body.AddForce(transform.up * speed * verticalAxis * Time.deltaTime, ForceMode.Acceleration);
+
         }
         else if (verticalAxis < 0.0f)
         {
-            body.velocity = Vector3.zero;
- 
+            //body.velocity = Vector3.zero;
+
+        }
+
+        //direction of the joysticks
+        Vector3 direct = new Vector3(RotHorizontalAxis, RotVerticalAxis, 0.0f).normalized;
+
+
+        if (verticalAxis != 0.0f || RotVerticalAxis != 0.0f)
+        {
+            body.rotation = Quaternion.Slerp(body.rotation, Quaternion.LookRotation(new Vector3(0, 0, 1), direct), 0.1f);
         }
 
 
-        //body.rotation = Quaternion.Euler(body.rotation.eulerAngles + new Vector3(0.0f, 0.0f, Mathf.Deg2Rad * -rotationSpeed * horizontalAxis));
 
-        Vector3 direct = new Vector3(RotHorizontalAxis, RotVerticalAxis, 0.0f).normalized;
+        Debug.Log(r);
 
-        //body.AddForce(direct * speed * Time.deltaTime, ForceMode.Acceleration);
-        body.rotation = Quaternion.Slerp(body.rotation, Quaternion.LookRotation(new Vector3(0, 0, 1), direct), 0.1f);
-
-
-
-        //transform.Rotate(r, Space.World);
-        Debug.Log(m);
-        //rotate = new Vector2(0, 0);
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player1.buttonEast.wasPressedThisFrame)
+        {
+            Debug.Log(player1);
+            Debug.Log("This works");
+        }
+        if(player1.leftStick.x.ReadValue() != 0 || player1.leftStick.y.ReadValue() != 0)
+        {
+            Debug.Log("ROTATION");
+            Vector2 r = new Vector2(player1.leftStick.x.ReadValue(), player1.leftStick.y.ReadValue());
+
+            float RotHorizontalAxis = r.x;
+            float RotVerticalAxis = r.y;
+
+            Vector3 direct = new Vector3(RotHorizontalAxis, RotVerticalAxis, 0.0f).normalized;
+            body.rotation = Quaternion.Slerp(body.rotation, Quaternion.LookRotation(new Vector3(0, 0, 1), direct), 0.1f);
+        }
+        //Movement();
+    }
+
+   
 
 
 }
