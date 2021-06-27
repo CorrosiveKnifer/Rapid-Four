e872d8bea0a53ee9a4fc345535fc53d570dc0463
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2021-06-27 15:07:48 +1200

    all my sunday work

--- Assets/Scripts/EndScreenScript.cs
+++ Assets/Scripts/EndScreenScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 /// <summary>
-/// William de Beer
+/// William de Beer, Rachael Colaco
 /// </summary>
 public class EndScreenScript : MonoBehaviour
 {
@@ -12,6 +12,13 @@ public class EndScreenScript : MonoBehaviour
     public Text Highscore;
     public Text Score;
 
+    public Text GameTime;
+
+    public Text Player1Kills;
+    public Text Player2Kills;
+    public Text Player1Deaths;
+    public Text Player2Deaths;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +26,12 @@ public class EndScreenScript : MonoBehaviour
         GetComponent<AudioAgent>().PlayBackground("EndMusic", true, 10);
         Score.text = ScoreToDisplay.ToString();
         Highscore.text = GameManager.HighScore.ToString();
+
+        GameTime.text = GameManager.GameTime.ToString();
+        Player1Kills.text = GameManager.GameTime.ToString();
+        Player2Kills.text = GameManager.GameTime.ToString();
+        Player1Deaths.text = GameManager.GameTime.ToString();
+        Player2Deaths.text = GameManager.GameTime.ToString();
+
     }
 }
